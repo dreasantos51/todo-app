fca5b659fa04e253058422521c73c842fb3ec48f
Andrea Santos <dreasantos51@gmail.com>
2023-06-07 17:21:26 -0400

    creando primero components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
+import React from "react";
+import {Title} from "./components/Title";
+
+const App = () => {
   return (
-    <div>
-       <h1 className="text-3xl font-bold underline">Todo APP</h1>
+    <div className="bg-gray-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
+      <div className="container flex flex-colmax-w-xl">
+        <Title />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <h1 className="text-5xl font-anton font-bold tracking-widest">Todo App</h1>
+  );
+};
+
+export { Title };
